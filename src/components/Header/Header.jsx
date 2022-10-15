2676893c512f546716ffc7224e3ba7fdcec67b2b
Galua Galua <galua@syneforge.com>
2022-10-15 21:21:12 +0300

    raw commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
 
 
 const Header = () => {
-    const tg = window.Telegram.WebApp
-
-    const onClose = () => tg.close()
+    const {onClose, user} = useTelegram()
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закруть</Button>
-            <span className={'username'}>{tg.initDataUnsafe?.user?.username}</span>
+            <span className={'username'}>{user?.username}</span>
         </div>
     );
 };
